jlaunonen/partyDecider

commit 0fa1b34af2c2b88171155a1ed96776cc66c52214

Fix frontend integration and watched dirs

backend/__main__.py
@@ -22,6 +22,7 @@ def main():
         host=settings.HOST,
         port=settings.PORT,
         reload=settings.DEBUG and not config.IS_ZIP_APP,
+        reload_dirs="backend",
     )
 
 

backend/frontend_integration.py
@@ -31,22 +31,25 @@ async def lifecycle():
         )
 
         def runner():
-            with contextlib.chdir("frontend"):
-                proc = subprocess.Popen(
-                    cmd, stdout=sys.stdout, stderr=sys.stderr, shell=True
-                )
-                while not interrupt and proc.poll() is None:
-                    try:
-                        proc.communicate(timeout=1)
-                    except subprocess.TimeoutExpired:
-                        pass
-                print("Stopping vite...")
-                proc.terminate()
+            proc = subprocess.Popen(
+                cmd,
+                stdout=sys.stdout,
+                stderr=sys.stderr,
+                shell=True,
+                cwd="frontend",
+            )
+            while not interrupt and proc.poll() is None:
                 try:
-                    proc.wait(2)
+                    proc.communicate(timeout=1)
                 except subprocess.TimeoutExpired:
-                    print("Killing vite...")
-                    proc.kill()
+                    pass
+            print("Stopping vite...")
+            proc.terminate()
+            try:
+                proc.wait(2)
+            except subprocess.TimeoutExpired:
+                print("Killing vite...")
+                proc.kill()
 
         thread = threading.Thread(target=runner, name="Vite")
         thread.start()

commit f4ce436f4473fecfdf96eda02fcdeeaa78f6ecc4

Fix a warning about directory watch in zipapp.
Fixes: 0fa1b34af2c2b88171155a1ed96776cc66c52214

backend/__main__.py
@@ -17,12 +17,13 @@ def main():
     # Expect: Environment variables are inherited to child processes.
     os.environ["X_SERV_PID"] = str(os.getpid())
 
+    use_reload = settings.DEBUG and not config.IS_ZIP_APP
     uvicorn.run(
         "backend.main:app",
         host=settings.HOST,
         port=settings.PORT,
-        reload=settings.DEBUG and not config.IS_ZIP_APP,
-        reload_dirs="backend",
+        reload=use_reload,
+        reload_dirs="backend" if use_reload else None,
     )
 
 